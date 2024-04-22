da0491915d8b024859bc6e6269394ad5371fb2ac
fedeholc <federicoholc@gmail.com>
2024-04-22 11:00:36 -0300

    Agregar verificación de login

---
+++ app/src/main/java/com/example/clubdeportivo/Club.kt
@@ -0,0 +1,28 @@
+package com.example.clubdeportivo
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+
+
+class MiBaseDeDatosHelper(context: Context) : SQLiteOpenHelper(context, NOMBRE_BD, null, VERSION_BD) {
+    companion object {
+        private const val NOMBRE_BD = "club.db"
+        private const val VERSION_BD = 1
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        // Crea la tabla aquí
+        //db.execSQL("CREATE TABLE libros (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, autor TEXT)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // Actualiza la estructura de la base de datos aquí
+       // if (oldVersion == 1 && newVersion == 2) {
+         //   db.execSQL("ALTER TABLE libros ADD COLUMN fecha_publicacion INTEGER")
+        //}
+    }
+}
+
--- app/src/main/java/com/example/clubdeportivo/MainActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.clubdeportivo
 
-import android.os.Bundle
+
+import android.content.Context
 import androidx.activity.enableEdgeToEdge
+import android.database.sqlite.SQLiteDatabase
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+@Suppress("SpellCheckingInspection")
 class MainActivity : AppCompatActivity() {
+    private lateinit var dbHelper: MiBaseDeDatosHelper
+    private lateinit var db: SQLiteDatabase
+
+    fun mostrarAlerta(context: Context, titulo: String, mensaje: String) {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle(titulo)
+        builder.setMessage(mensaje)
+        builder.setPositiveButton("Aceptar") { dialog, which ->
+            // Acción al hacer clic en el botón Aceptar (si es necesario)
+        }
+        builder.show()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +36,47 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        dbHelper = MiBaseDeDatosHelper(this)
+        db = dbHelper.writableDatabase
+
+        val buttonAcceder: Button =findViewById(R.id.btnAcceder)
+
+        // add toast to button
+        buttonAcceder.setOnClickListener {
+
+            // get input text values
+            val usuario = findViewById<EditText>(R.id.editTextUsuario).text.toString()
+            val password = findViewById<EditText>(R.id.editTextPassword).text.toString()
+
+            val cursor = db.rawQuery("SELECT * FROM usuario", null)
+            var loginCorrecto = false
+
+            while (cursor.moveToNext()) {
+                val codUsu = cursor.getInt(0)
+                val nombreUsu = cursor.getString(1)
+                val passUsu = cursor.getString(2)
+                //Log.d("MiBaseDeDatos", "Libro: $id - $titulo - $autor")
+                if (usuario == nombreUsu && password == passUsu){
+                    loginCorrecto = true
+                    break
+                }
+            }
+            if (loginCorrecto){
+                //Toast.makeText(this, "Usuario correcto", Toast.LENGTH_SHORT).show()
+            }else{
+                //Toast.makeText(this, "Usuario incorrecto", Toast.LENGTH_SHORT).show()
+                mostrarAlerta(this, "Error de inicio de sesión", "El nombre de usuario o la contraseña son incorrectos.")
+
+            }
+
+            // Cerrar el cursor y la base de datos
+            //cursor.close()
+            // db.close()
+
+        }
     }
+
+
+
 }
