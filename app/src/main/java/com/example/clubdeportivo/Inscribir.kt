cea127e1197a3a6b3d35af1334d68c27f404ee5b
fedeholc <federicoholc@gmail.com>
2024-04-23 10:30:11 -0300

    Agregar radio buttons a inscripción

--- app/src/main/java/com/example/clubdeportivo/Inscribir.kt
+++ app/src/main/java/com/example/clubdeportivo/Inscribir.kt
@@ -1,36 +1,20 @@
 package com.example.clubdeportivo
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MenuPrincipal : AppCompatActivity() {
+class Inscribir : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu_principal)
+        setContentView(R.layout.activity_inscribir)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val txtUsuario: TextView = findViewById(R.id.txtUsuario)
-        txtUsuario.text = "Usuario:  ${DatosCompartidos.usuarioLogueado}"
-
-
-        val btnInscribir: Button =findViewById(R.id.btnInscribir)
-
-        btnInscribir.setOnClickListener {
-            val intentar = Intent(this, Inscribir::class.java)
-            startActivity(intentar)
-        }
-
     }
-
-
 }
