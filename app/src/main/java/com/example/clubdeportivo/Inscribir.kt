1575ea3197742cc6a8f7d329089cf88a381f3c58
fedeholc <federicoholc@gmail.com>
2024-04-23 14:12:36 -0300

    Agregar campos al formulario de inscripción

--- app/src/main/java/com/example/clubdeportivo/Inscribir.kt
+++ app/src/main/java/com/example/clubdeportivo/Inscribir.kt
@@ -1,12 +1,19 @@
 package com.example.clubdeportivo
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class Inscribir : AppCompatActivity() {
+    private lateinit var editTextDate: EditText
+    private lateinit var calendar: Calendar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,36 @@ class Inscribir : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        editTextDate = findViewById(R.id.editTextDate)
+        calendar = Calendar.getInstance()
+
+        editTextDate.setOnClickListener {
+            showDatePickerDialog()
+        }
+    }
+    private fun showDatePickerDialog() {
+        val currentYear = calendar.get(Calendar.YEAR)
+        val currentMonth = calendar.get(Calendar.MONTH)
+        val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                // Maneja la fecha seleccionada
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, month)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                val selectedDate = dateFormat.format(calendar.time)
+
+                editTextDate.setText(selectedDate)
+            },
+            currentYear,
+            currentMonth,
+            currentDay
+        )
+
+        datePickerDialog.show()
     }
 }
