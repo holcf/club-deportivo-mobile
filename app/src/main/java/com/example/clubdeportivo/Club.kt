7f07051dc59b667747bfcf04aab7caf2fef5ce20
fedeholc <federicoholc@gmail.com>
2024-04-24 14:55:39 -0300

    Agregar creación de la base de manera dinámica si no existe

--- app/src/main/java/com/example/clubdeportivo/Club.kt
+++ app/src/main/java/com/example/clubdeportivo/Club.kt
@@ -1,7 +1,6 @@
 package com.example.clubdeportivo
 
-import android.content.ContentValues
-import android.content.Context
+ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
@@ -11,7 +10,46 @@ class DatosCompartidos {
     }
 }
 
+object BaseDatos {
+      fun crear(db: SQLiteDatabase){
+        // BASE DE DATOS
+        // Crear tabla roles
+        db.execSQL("CREATE TABLE IF NOT EXISTS roles (RolUsu INTEGER PRIMARY KEY, NomRol TEXT);")
 
+        // Insertar datos en la tabla roles
+        db.execSQL("INSERT INTO roles (RolUsu, NomRol) VALUES (120, 'Administrador'), (121, 'Empleado');")
+
+        // Crear tabla usuario
+        db.execSQL("CREATE TABLE IF NOT EXISTS usuario (CodUsu INTEGER PRIMARY KEY AUTOINCREMENT, NombreUsu TEXT, PassUsu TEXT, RolUsu INTEGER, Activo INTEGER DEFAULT 1, FOREIGN KEY (RolUsu) REFERENCES roles(RolUsu));")
+
+        // Insertar datos en la tabla usuario
+        db.execSQL("INSERT INTO usuario (NombreUsu, PassUsu, RolUsu) VALUES ('Admin', '1234', 120);")
+
+        // Crear tabla socio
+        db.execSQL("CREATE TABLE IF NOT EXISTS socio (NSocio INTEGER PRIMARY KEY, Nombre TEXT, DNI INTEGER, Correo TEXT, FechaInscripcion TEXT, AptoFisico INTEGER);")
+
+        // Insertar datos en la tabla socio
+        db.execSQL("INSERT INTO socio (NSocio, Nombre, DNI, Correo, FechaInscripcion, AptoFisico) VALUES (1, 'Socio 1', 11111111, 's1@unmail.com', '2023-10-24', 1), (2, 'Socio 2', 22222222, 's2@unmail.com', '2023-10-24', 1), (3, 'Socio 3', 33333333, 's3@unmail.com', '2023-10-24', 1);")
+
+        // Crear tabla nosocio
+        db.execSQL("CREATE TABLE IF NOT EXISTS nosocio (NSocio INTEGER PRIMARY KEY, Nombre TEXT, DNI INTEGER, Correo TEXT, FechaInscripcion TEXT, AptoFisico INTEGER);")
+
+        // Insertar datos en la tabla nosocio
+        db.execSQL("INSERT INTO nosocio (NSocio, Nombre, DNI, Correo, FechaInscripcion, AptoFisico) VALUES (1, 'No Socio 1', 11234567, 'ns1@unmail.com', '2023-10-24', 1), (2, 'No Socio 2', 22345678, 'ns2@unmail.com', '2023-10-24', 1), (3, 'No Socio 3', 33456789, 'ns3@unmail.com', '2023-10-24', 1);")
+
+        // Crear tabla cuotaSocio
+        db.execSQL("CREATE TABLE IF NOT EXISTS cuotaSocio (IdCuota INTEGER PRIMARY KEY AUTOINCREMENT, NSocio INTEGER, Monto INTEGER, FechaPago TEXT, MetodoPago INTEGER, FechaInicio TEXT, Vencimiento TEXT, FOREIGN KEY (NSocio) REFERENCES socio(NSocio));")
+
+        // Insertar datos en la tabla cuotaSocio
+        db.execSQL("INSERT INTO cuotaSocio (NSocio, Monto, FechaPago, MetodoPago, FechaInicio, Vencimiento) VALUES (1, 3000, '2023-10-13', 1, '2023-10-13', '2023-11-13'), (2, 3000, '2023-10-13', 1, '2023-10-13', '2023-11-13'), (2, 3000, '2023-11-14', 1, '2023-11-14', '2023-12-14');")
+
+        // Crear tabla cuotaDiaria
+        db.execSQL("CREATE TABLE IF NOT EXISTS cuotaDiaria (IdCuota INTEGER PRIMARY KEY AUTOINCREMENT, NSocio INTEGER, Monto INTEGER, FechaPago TEXT, MetodoPago INTEGER, FechaInicio TEXT, Vencimiento TEXT, FOREIGN KEY (NSocio) REFERENCES nosocio(NSocio));")
+
+        // Insertar datos en la tabla cuotaDiaria
+        db.execSQL("INSERT INTO cuotaDiaria (NSocio, Monto, FechaPago, MetodoPago, FechaInicio, Vencimiento) VALUES (1, 400, '2023-10-24', 1, '2023-10-24', '2023-11-24');")
+    }
+}
 class MiBaseDeDatosHelper(context: Context) : SQLiteOpenHelper(context, NOMBRE_BD, null, VERSION_BD) {
     companion object {
         private const val NOMBRE_BD = "club.db"
@@ -21,6 +59,7 @@ class MiBaseDeDatosHelper(context: Context) : SQLiteOpenHelper(context, NOMBRE_B
     override fun onCreate(db: SQLiteDatabase) {
         // Crea la tabla aquí
         //db.execSQL("CREATE TABLE libros (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, autor TEXT)")
+        BaseDatos.crear(db)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
