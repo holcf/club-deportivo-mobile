beb1e4a723ab14f19801513584ee5170c1b9edcb
Federico Holc <federicoholc@gmail.com>
2024-06-02 19:54:38 -0300

    fix warnings

--- app/src/main/java/com/example/clubdeportivo/CobroCuotas.kt
+++ app/src/main/java/com/example/clubdeportivo/CobroCuotas.kt
@@ -1,41 +1,20 @@
 package com.example.clubdeportivo
 
-import android.content.Context
+
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
+
+ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
-import android.widget.Spinner
-import android.widget.TextView
-import android.widget.Toast
+
+
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SocioSpinnerAdapter(context: Context, socios: List<Socio>) :
-    ArrayAdapter<Socio>(context, 0, socios) {
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return createViewFromResource(position, convertView, parent, android.R.layout.simple_spinner_item)
-    }
-
-    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return createViewFromResource(position, convertView, parent, android.R.layout.simple_spinner_dropdown_item)
-    }
-
-    private fun createViewFromResource(position: Int, convertView: View?, parent: ViewGroup, resource: Int): View {
-        val view = convertView ?: LayoutInflater.from(context).inflate(resource, parent, false)
-        val textView = view.findViewById<TextView>(android.R.id.text1)
-        val socio = getItem(position)
-        textView.text = socio?.nombre
-        return view
-    }
-}
 class CobroCutas : AppCompatActivity() {
     private lateinit var dbHelper: MiBaseDeDatosHelper
     private lateinit var db: SQLiteDatabase
@@ -79,7 +58,9 @@ class CobroCutas : AppCompatActivity() {
         autoCompleteTextView.setOnItemClickListener { parent, view, position, id ->
             val selectedInfo = parent.getItemAtPosition(position) as String
             selectedSocio = socios.find { "${it.dni} - ${it.nombre}" == selectedInfo }
-            //Toast.makeText(this@CobroCutas, "Seleccionaste: ${selectedSocio?.nombre} (ID: ${selectedSocio?.nroSocio})", Toast.LENGTH_SHORT).show()
+
+            //luego para hacer el cobro usar selectedSocio.nroSocio
+
         }
         autoCompleteTextView.validator = object : AutoCompleteTextView.Validator {
             override fun isValid(text: CharSequence?): Boolean {
@@ -96,20 +77,7 @@ class CobroCutas : AppCompatActivity() {
                 autoCompleteTextView.performValidation()
             }
         }
-        /* spinner = findViewById(R.id.spnSocio)
-        val adapter = SocioSpinnerAdapter(this, socios)
-        spinner.adapter = adapter
 
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                val selectedSocio = parent.getItemAtPosition(position) as Socio
-                Toast.makeText(this@CobroCutas, "Seleccionaste: ${selectedSocio.nombre} (ID: ${selectedSocio.nroSocio})", Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>) {
-                // No hacer nada
-            }
-        }*/
 
     }
 }
